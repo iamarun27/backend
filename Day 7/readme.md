f8184f8886cadf512ca3df10bbeffbc13168d795
Arun Kumar <arunkumaryadav2705@gmail.com>
2026-07-02 12:31:32 +0530

    day-7

---
+++ Day 7/readme.md
@@ -0,0 +1,17 @@
+CRUD in Database
+
+
+arun  CX6DcLtXmgSpzjfu
+
+mongodb+srv://arun:CX6DcLtXmgSpzjfu@cluster0.s92bchb.mongodb.net/
+http://localhost:3000
+
+schema
+
+note 
+{
+  title = string
+  description = string
+}
+
+crud operation perform krns k liye
---
+++ Day 7/src/app.js
@@ -0,0 +1,41 @@
+// server create krna
+
+const express = require("express");
+const noteModel = require("./models/notes.model");
+
+const app = express();
+
+app.use(express.json());
+
+// post
+// req.body = title,description
+
+app.post("/notes", async (req, res) => {
+  const { title, description, age } = req.body;
+  const note = await noteModel.create({
+    title,
+    description,
+    age,
+  });
+
+  res.status(201).json({
+    message: "note created successfully",
+    note,
+  });
+});
+
+// get
+// fetch all the data
+
+app.get("/notes", async (req, res) => {
+  const notes = await noteModel.find();
+
+  res.status(200).json({
+    message: "Notes fetched successfully",
+    notes,
+  });
+});
+
+
+
+module.exports = app;
