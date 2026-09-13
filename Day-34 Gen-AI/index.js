6189450f5cd516b7c5ed6d6a9b41c2b07e255639
Arun Kumar <arunkumaryadav2705@gmail.com>
2026-09-13 14:22:04 +0530

    GenAI-Tools

--- Day-34 Gen-AI/index.js
+++ Day-34 Gen-AI/index.js
@@ -1,7 +1,23 @@
 import "dotenv/config";
 import readline from "readline/promises";
 import { ChatMistralAI } from "@langchain/mistralai";
-import {HumanMessage } from "langchain";
+import { HumanMessage, createAgent, tool } from "langchain";
+import { sendEmail } from "./mail.service.js";
+import * as z from "zod";
+
+const emailTool = tool(
+  sendEmail,
+
+  {
+    name: "emailTool",
+    description: "use this tool to send an emil",
+    schema: {
+      to: z.string().describe("The recipient's email address"),
+      html: z.string().describe("The HTML content of the email"),
+      subject: z.string().describe("The subject of the email"),
+    },
+  },
+);
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -13,20 +29,25 @@ const model = new ChatMistralAI({
   model: "mistral-small-latest",
 });
 
-const messages  = []
+const agent = createAgent({
+  model,
+  tools: [emailTool],
+});
+
+const messages = [];
 
 const getTimestamp = () => new Date().toLocaleTimeString();
 
 while (true) {
   const userInput = await rl.question("\n👤 You: ");
 
-  messages.push(new HumanMessage(userInput)); 
+  messages.push(new HumanMessage(userInput));
 
-  if (!userInput.trim()) continue;
+  // if (!userInput.trim()) continue;
 
-  const response = await model.invoke(messages);
+  const response = await agent.invoke({ messages });
 
-  messages.push(response);
+  messages.push(response.messages[response.messages.length - 1]);
   console.log(`\n🤖 AI [${getTimestamp()}]:\n${response.content}\n`);
 }
 
